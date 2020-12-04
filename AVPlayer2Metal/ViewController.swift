bf1d571d4a5a8c2509a19c6a6e2d98f66aa62971
lisyunn <lisyunn@gmail.com>
2020-12-05 08:25:12 +0900

    Initial Commit

---
+++ AVPlayer2Metal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayer2Metal
+//
+//  Created by lisyunn on 2020/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
